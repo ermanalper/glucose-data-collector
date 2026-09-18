6493d9ac781272c5802efb02acede7063b42df5e
ermanalper <ermanalper6@gmail.com>
2026-09-18 15:46:47 +0300

    ss kicking

--- app/infrastructure/broadcasters/memory_sse_broadcaster.py
+++ app/infrastructure/broadcasters/memory_sse_broadcaster.py
@@ -7,7 +7,6 @@ from app.infrastructure.interfaces.sse_broadcaster_interface import ISSEBroadcas
 class MemorySSEBroadcaster(ISSEBroadcaster):
     def __init__(self):
         self._queues: Set[Tuple[asyncio.AbstractEventLoop, asyncio.Queue, str]] = set()
-        #last ping times for each client
         self._last_heartbeats: Dict[str, float] = {}
 
     async def subscribe(self, client_name: str) -> AsyncGenerator[dict, None]:
@@ -20,7 +19,11 @@ class MemorySSEBroadcaster(ISSEBroadcaster):
 
         try:
             while True:
-                yield await queue.get()
+                msg = await queue.get()
+                # shut down the tunnel if None is sent
+                if msg is None:
+                    break
+                yield msg
         finally:
             self._queues.discard(item)
             self._last_heartbeats.pop(client_name, None)
@@ -33,21 +36,23 @@ class MemorySSEBroadcaster(ISSEBroadcaster):
             )
 
     def heartbeat(self, client_name: str) -> None:
-        # update last ping time for each client
         if client_name in self._last_heartbeats:
             self._last_heartbeats[client_name] = time.time()
 
     def get_active_clients(self) -> List[str]:
         current_time = time.time()
-        timeout_seconds = 40.0  # remove all clients from listening's list
+        timeout_seconds = 40.0
 
         dead_clients = [c for c, t in self._last_heartbeats.items() if current_time - t > timeout_seconds]
 
         for dead in dead_clients:
             self._last_heartbeats.pop(dead, None)
 
+            # kill the connection of the zombie
             items_to_remove = [item for item in self._queues if item[2] == dead]
             for item in items_to_remove:
-                self._queues.discard(item)
+                loop, queue, _ = item
+                # put none in the queue so subscribe kills the connection
+                loop.call_soon_threadsafe(queue.put_nowait, None)
 
         return list(self._last_heartbeats.keys())
